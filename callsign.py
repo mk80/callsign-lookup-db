78d8bb3c64d3a877ba338eb29770fb666e5a9e07
mk80 <michaelkosko@gmail.com>
2019-05-18 20:00:26 -0700

    Functions for database insert and query are in place but need debugging. Need to also work on invalid callsign or non-existant callsign entry from user.

--- callsign.py
+++ callsign.py
@@ -74,17 +74,57 @@ def verifyCallsignTable(callsignQ):
 # create callsign table and columns
 def addCallsign(callsignQ):
 	returnVal = False
+	tableName = None
+	createStmt = None
+	# check for table
 	returnVal = verifyCallsignTable(callsignQ)
-
+	# create table for callsign if it doesn't exist
 	if ( returnVal == False ):
-		insertStmt = 
+		tableName = "callsign." + callsignQ
+		createStmt = "CREATE TABLE " + tableName + " (colname varchar(50), coltimestamp varchar(30), colband varchar(3), colcomment varchar(255));"
+		returnVal = writeRecord(createStmt)
+	return returnVal
 
 
 # store contact info into callsign table
-def storeContact(name, timestamp, band, comment):
+def storeContact(callsignQ, nameQ, timestampQ, bandQ, commentQ):
+	returnVal = False
+	tableName = None
+	insertStmt = None
+	tableName = "callsign." + callsignQ
+	insertStmt = """INSERT into %s (colname, coltimestamp, colband, colcomment)
+		VALUES ('%s', '%s', '%s', '%s')
+		""" % (tableName, nameQ, timestampQ, bandQ, commentQ)
+	returnVal = writeRecord(insertStmt)
+	return returnVal
 
 # retrieve callsign info
 def retrieveContact(callsignQ):
+	returnVal = False
+	queryStmt = None
+	# check for table
+	returnVal = verifyCallsignTable(callsignQ)
+	if ( returnVal == True ):
+		queryStmt = """SELECT * FROM %s """ % (callsignQ)
+		returnVal = readRecord(queryStmt)
+		return returnVal
+	else:
+		return returnVal
+
+# drop table for callsign
+def removeContact(callsignQ):
+	returnVal = False
+	tableName = None
+	dropStmt = None
+	# check for table
+	returnVal = verifyCallsignTable(callsignQ)
+	if ( returnVal == True ):
+		tableName = "callsign." + callsignQ
+		dropStmt = """DROP TABLE %s""" % (tableName)
+		returnVal = writeRecord(dropStmt)
+		return returnVal
+	else:
+		return returnVal
 
 ## print json function
 def outputPrint(data):
@@ -98,72 +138,89 @@ def outputPrint(data):
 
 ### main ###
 
-affInput = {'Y','y','Yes','yes','YES'}
-negInput = {'N','n','No','no','NO'}
+if ( __name__ == "__main__"):
 
-callsign = 'run'
+	affInput = {'Y','y','Yes','yes','YES'}
+	negInput = {'N','n','No','no','NO'}
 
-print("\n Enter an FCC callsign to lookup or 0 to exit.\n\n")
+	callsign = 'run'
+	returnVal = None
 
-## run loop
-while (callsign != '0'):
-	# query for callsign
-	callsign = input("Callsign : ")
-	print('\n')
+	print("\n Enter an FCC callsign to lookup or 0 to exit.\n\n")
 
-	# move on to exit if callsign == '0'
-	if (callsign != '0'):
-		# build request url for api to callook.info
-		apiGet =  'https://callook.info/' + callsign + '/json'
-
-		# GET to callook.info for callsign info
-		r = requests.get(apiGet)
-
-		output = r.json()
-
-		# print result
-		outputPrint(output)
+	## run loop
+	while (callsign != '0'):
+		# query for callsign
+		callsign = input("Callsign : ")
 		print('\n')
 
-		inputContact = 'q'
+		# move on to exit if callsign == '0'
+		if (callsign != '0'):
+			# build request url for api to callook.info
+			apiGet =  'https://callook.info/' + callsign + '/json'
 
-		while (inputContact not in affInput and inputContact not in negInput):
-			inputContact = input("Would you like to record this contact? ")
+			# GET to callook.info for callsign info
+			r = requests.get(apiGet)
+
+			output = r.json()
+
+			# print result
+			outputPrint(output)
 			print('\n')
+			
+			callsignQ = output['current']['callsign']
+			returnVal = retrieveContact(callsignQ)
+			if ( returnVal != False ):
+				print("Past contact with " + callsignQ + ":\n")
+				print(returnVal)
+
+			inputContact = 'q'
+
+			while (inputContact not in affInput and inputContact not in negInput):
+				inputContact = input("Would you like to record this contact? ")
+				print('\n')
+
+				if (inputContact in affInput):
+					nameQ = output['name']
+					timestampQ = datetime.datetime.now()
+					print("Select band: \n")
+					print(" 1. HF\n")
+					print(" 2. VHF\n")
+					print(" 3. UHF\n")
+					bandQ = input(": ")
+					if ( bandQ == '1'): bandQ = 'HF'
+					elif (bandQ == '2'): bandQ = 'VHF'
+					else: bandQ = 'UHF'
+					print('\n')
+					commentQ = input("Comments: ")
+				elif (inputContact not in negInput):
+					print('Please enter y/n\n')
 
 			if (inputContact in affInput):
-				callsignQ = output['current']['callsign']
-				nameQ = output['name']
-				timestampQ = datetime.datetime.now()
-				print("Select band: \n")
-				print(" 1. HF\n")
-				print(" 2. VHF\n")
-				print(" 3. UHF\n")
-				bandQ = input(": ")
-				if ( bandQ == '1'): bandQ = 'HF'
-				elif (bandQ == '2'): bandQ = 'VHF'
-				else: bandQ = 'UHF'
-				print('\n')
-				commentQ = input("Comments: ")
-			elif (inputContact not in negInput):
-				print('Please enter y/n\n')
+				returnVal = None
+				returnVal = addCallsign(callsignQ)
+				if ( returnVal == None ):
+					returnVal = storeContact(callsignQ, nameQ, timestampQ, bandQ, commentQ)
+					if ( returnVal == None ):
+						print("Contact successfully added!\n")
+						
 
-		if (inputContact in affInput):
-			print(callsignQ)
-			print(nameQ)
-			print(timestampQ)
-			print(bandQ)
-			print(commentQ)
 
-		# test db connection and write/read
-		#writeRecord("CREATE TABLE callsign.test (coltest varchar(20));")
-		#writeRecord("INSERT into callsign.test (coltest) values ('It works!');")
-		#dbtest = readRecord("SELECT * FROM callsign.test;")
-		#print('dbtest = ' + str(dbtest))
-		#writeRecord("DROP TABLE callsign.test;")
-		
-	else:
-		# exit message
-		print('exit')
+			#print(callsignQ)
+			#print(nameQ)
+			#print(timestampQ)
+			#print(bandQ)
+			#print(commentQ)
+
+			# unit test db connection and write/read
+			#writeRecord("CREATE TABLE callsign.test (coltest varchar(20));")
+			#writeRecord("INSERT into callsign.test (coltest) values ('It works!');")
+			#dbtest = readRecord("SELECT * FROM callsign.test;")
+			#print('dbtest = ' + str(dbtest))
+			#writeRecord("DROP TABLE callsign.test;")
+			
+		else:
+			# exit message
+			print('exit')
 
 exit
